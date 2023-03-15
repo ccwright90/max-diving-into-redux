2d4eb33ad167fc29f1c2c5102bc4dbe0b243d670
NCASS\chris.wright <chris.wright@ncass.org.uk>
2023-03-15 14:15:37 +0000

    multiple slices

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,22 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { authActions } from '../store/index';
+
 import classes from './Auth.module.css';
 
+
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.login());
+  }
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
@@ -13,7 +25,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button type="submit" onClick={loginHandler}>Login</button>
         </form>
       </section>
     </main>
