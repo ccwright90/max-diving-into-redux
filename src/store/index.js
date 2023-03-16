e8ddec1c1169e967975f0e9af5d33bdfae41526e
NCASS\chris.wright <chris.wright@ncass.org.uk>
2023-03-16 12:13:10 +0000

    splitting up our logic into separate files

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,11 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialCounterState = { counter: 0, showCounter: true };
+import counterReducer from './counter';
+import authReducer from './auth';
 
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        toggle(state) {
-            state.showCounter = !state.showCounter;
-        }
-    }
-});
 
-const initialAuthState = {
-    isAuthenticated: false
-};
+const store = configureStore({ reducer: { counter: counterReducer, auth: authReducer} });
 
-const authSlice = createSlice({
-    name: 'authentication',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true;
-        },
-        logout(state) {
-            state.isAuthenticated = false;
-        }
-    }
-})
 
-const store = configureStore({ reducer: { counter: counterSlice.reducer, auth: authSlice.reducer} });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
